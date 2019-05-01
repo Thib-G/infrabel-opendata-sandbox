454d6dbb768838635c85f0963c6fe57b4dc6560a
Thib <thibaut.g@protonmail.com>
2019-05-02 00:09:50 +0200

    Add source

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,13 @@
     <div>
       <button @click="animate()">Animate</button>
     </div>
+    <h3>Sources</h3>
+    <p>Infrabel Open Data: &laquo;&nbsp;<a href="https://opendata.infrabel.be/explore/dataset/geovoies/"
+      target="_blank">Liste 
+      et position géographique des voies principales</a>&nbsp;&raquo;.</p>
+    <p>Atlas de Belgique: <a href="http://www.atlas-belgique.be/cms2/index.php?page=cartodata_fr"
+      target="_blank">régions</a>.</p>
+    <hr />
   </div>
 </template>
 
