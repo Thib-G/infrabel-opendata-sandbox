bfff8b9a318a5452b08ecda0c18f2b26bc2f2e8e
Thib <thibaut.g@protonmail.com>
2019-05-01 23:04:11 +0200

    Add d3-* to transpile

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/infrabel-opendata-sandbox/'
     : '/',
+  transpileDependencies: [
+    'd3-selection',
+    'd3-geo',
+    'd3-scale',
+    'd3-transition',
+  ],
 };
