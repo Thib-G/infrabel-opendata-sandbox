5d8df7589c34acd29bf698715a002a6a2eb622de
Thibaut Goelff <thibaut@example.com>
2021-04-06 01:44:40 +0200

    Add regexp to transpile d3-*

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,7 @@ module.exports = {
     ? '/'
     : '/',
   transpileDependencies: [
-    'd3-selection',
-    'd3-geo',
-    'd3-scale',
-    'd3-transition',
-    'd3-collection',
-    'd3-array',
-    'd3-scale-chromatic',
-    'd3-interpolate',
-    'd3-format',
-    'd3-time',
-    'd3-time-format',
+    /d3-.+/,
     'internmap',
   ],
 };
